1bd7e4e4adda210242197028227ad2ba3e50bd08
Steven Robinett <Steven.robinett@gfcmsu.edu>
2024-02-05 17:52:14 -0700

    init upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="layout.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="banner">
+        Placeholder - steverobinett.net
+    </div>
+    <div class="container">
+        <div class="column-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas deserunt cum aliquid asperiores aspernatur, omnis ea consectetur labore dolores quibusdam ipsam? Obcaecati accusamus dicta omnis incidunt consequatur error nihil repellendus?</div>
+        <div class="column">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, facere veniam. Ab sed sunt distinctio repudiandae voluptates nemo dolorum, iusto eos dicta blanditiis quia eligendi ducimus veniam deserunt. Autem, tempora.</div>
+        <div class="column-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quia, obcaecati ex eveniet quae aspernatur nemo impedit fugiat ducimus accusantium suscipit id quaerat minus facilis corrupti, incidunt minima atque veniam?</div>
+        <!-- <div class="column">Column 4</div>
+        <div class="column">Column 5</div> -->
+    </div>
+</body>
+</html>
